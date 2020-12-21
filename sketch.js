f66c10d6e0123bec2fc066f6259d68f8aa8f5cec
availablewow <dubey.aditya.official@gmail.com>
2020-12-21 21:07:52 +0530

    platform

--- sketch.js
+++ sketch.js
@@ -17,6 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    ground2 = new Ground(100,300,300,300)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -48,7 +49,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
-
+    ground2.display();
     box3.display();
     box4.display();
     pig3.display();
